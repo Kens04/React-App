a427b9d57c907ff21d6d4aeea34d2f04edd109f9
Kens04 <kento090403@icloud.com>
2025-02-20 23:31:37 +0900

    filterからfindに変更し、記事が見つからない場合は「記事が見つかりません」を表示

--- src/posts/PostDetail.jsx
+++ src/posts/PostDetail.jsx
@@ -3,23 +3,23 @@ import { posts } from "../data/posts";
 
 export const PostDetail = () => {
   const { id } = useParams();
-  const post = posts.filter((post) => post.id === Number(id));
-  const { title, thumbnailUrl, createdAt, categories, content } = post[0];
+  const post = posts.find((post) => post.id === Number(id));
+  if (!post) return <div>記事が見つかりません</div>;
 
   return (
     <div className="max-w-3xl mx-auto">
       <div>
         <div>
-          <img src={thumbnailUrl} alt={title} />
+          <img src={post.thumbnailUrl} alt={post.title} />
         </div>
         <div className="p-4">
           <div className="flex justify-between">
             <div className="text-slate-400 text-sm">
-              {new Date(createdAt).toLocaleDateString()}
+              {new Date(post.createdAt).toLocaleDateString()}
             </div>
             <div>
               <div className="flex gap-2">
-                {categories.map((category, index) => (
+                {post.categories.map((category, index) => (
                   <div
                     key={index}
                     className="border border-blue-500 p-1 text-sm rounded text-blue-500"
@@ -30,9 +30,9 @@ export const PostDetail = () => {
               </div>
             </div>
           </div>
-          <p className="text-2xl text-left mt-4 text-slate-900">{title}</p>
+          <p className="text-2xl text-left mt-4 text-slate-900">{post.title}</p>
           <div className="mt-4 text-left text-slate-700">
-            <div dangerouslySetInnerHTML={{ __html: content }} />
+            <div dangerouslySetInnerHTML={{ __html: post.content }} />
           </div>
         </div>
       </div>
