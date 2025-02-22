ea0924aa518549e420aab435e9c7c74a013251ab
Kens04 <kento090403@icloud.com>
2025-02-22 14:23:47 +0900

    Loading処理を三項演算子から早期リターンへリファクタリング

--- src/posts/PostDetail.jsx
+++ src/posts/PostDetail.jsx
@@ -24,46 +24,41 @@ export const PostDetail = () => {
     fetchData();
   }, []);
 
+  if (isLoading) return <div>読み込み中...</div>;
   if (!posts) return <div>記事が見つかりません</div>;
 
   return (
-    <>
-      {isLoading ? (
-        <div>読み込み中...</div>
-      ) : (
-        <div className="max-w-3xl mx-auto">
-          <div>
-            <div>
-              <img src={posts.thumbnailUrl} alt={posts.title} />
+    <div className="max-w-3xl mx-auto">
+      <div>
+        <div>
+          <img src={posts.thumbnailUrl} alt={posts.title} />
+        </div>
+        <div className="p-4">
+          <div className="flex justify-between">
+            <div className="text-slate-400 text-sm">
+              {new Date(posts.createdAt).toLocaleDateString()}
             </div>
-            <div className="p-4">
-              <div className="flex justify-between">
-                <div className="text-slate-400 text-sm">
-                  {new Date(posts.createdAt).toLocaleDateString()}
-                </div>
-                <div>
-                  <div className="flex gap-2">
-                    {posts.categories.map((category, index) => (
-                      <div
-                        key={index}
-                        className="border border-blue-500 p-1 text-sm rounded text-blue-500"
-                      >
-                        {category}
-                      </div>
-                    ))}
+            <div>
+              <div className="flex gap-2">
+                {posts.categories.map((category, index) => (
+                  <div
+                    key={index}
+                    className="border border-blue-500 p-1 text-sm rounded text-blue-500"
+                  >
+                    {category}
                   </div>
-                </div>
-              </div>
-              <p className="text-2xl text-left mt-4 text-slate-900">
-                {posts.title}
-              </p>
-              <div className="mt-4 text-left text-slate-700">
-                <div dangerouslySetInnerHTML={{ __html: posts.content }} />
+                ))}
               </div>
             </div>
           </div>
+          <p className="text-2xl text-left mt-4 text-slate-900">
+            {posts.title}
+          </p>
+          <div className="mt-4 text-left text-slate-700">
+            <div dangerouslySetInnerHTML={{ __html: posts.content }} />
+          </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
