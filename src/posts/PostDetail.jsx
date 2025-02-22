76be584bd1ab08a7d301d81df087e08240d7a030
Kens04 <kento090403@icloud.com>
2025-02-22 12:10:49 +0900

    APIから記事データを取得し、記事表示とローディング機能を実装

--- src/posts/PostDetail.jsx
+++ src/posts/PostDetail.jsx
@@ -1,41 +1,69 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { posts } from "../data/posts";
 
 export const PostDetail = () => {
   const { id } = useParams();
-  const post = posts.find((post) => post.id === Number(id));
-  if (!post) return <div>記事が見つかりません</div>;
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+      );
+      const data = await res.json();
+      setPosts(data.post);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!posts) return <div>記事が見つかりません</div>;
 
   return (
-    <div className="max-w-3xl mx-auto">
-      <div>
-        <div>
-          <img src={post.thumbnailUrl} alt={post.title} />
-        </div>
-        <div className="p-4">
-          <div className="flex justify-between">
-            <div className="text-slate-400 text-sm">
-              {new Date(post.createdAt).toLocaleDateString()}
-            </div>
+    <>
+      {isLoading ? (
+        <div>読み込み中...</div>
+      ) : (
+        <div className="max-w-3xl mx-auto">
+          <div>
             <div>
-              <div className="flex gap-2">
-                {post.categories.map((category, index) => (
-                  <div
-                    key={index}
-                    className="border border-blue-500 p-1 text-sm rounded text-blue-500"
-                  >
-                    {category}
+              <img src={posts.thumbnailUrl} alt={posts.title} />
+            </div>
+            <div className="p-4">
+              <div className="flex justify-between">
+                <div className="text-slate-400 text-sm">
+                  {new Date(posts.createdAt).toLocaleDateString()}
+                </div>
+                <div>
+                  <div className="flex gap-2">
+                    {posts.categories.map((category, index) => (
+                      <div
+                        key={index}
+                        className="border border-blue-500 p-1 text-sm rounded text-blue-500"
+                      >
+                        {category}
+                      </div>
+                    ))}
                   </div>
-                ))}
+                </div>
+              </div>
+              <p className="text-2xl text-left mt-4 text-slate-900">
+                {posts.title}
+              </p>
+              <div className="mt-4 text-left text-slate-700">
+                <div dangerouslySetInnerHTML={{ __html: posts.content }} />
               </div>
             </div>
           </div>
-          <p className="text-2xl text-left mt-4 text-slate-900">{post.title}</p>
-          <div className="mt-4 text-left text-slate-700">
-            <div dangerouslySetInnerHTML={{ __html: post.content }} />
-          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
